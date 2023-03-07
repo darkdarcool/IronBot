da3dc2cdbc78f3339e448263d33d63ef07dba2be
darkdarcool <darkdarcool@gmail.com>
2023-03-06 16:10:56 -0800

    Oops

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { loadEnv } from "./utils/loadenv.js";
 loadEnv();
 import Bot from "./bot.js";
 const bot = new Bot(process.env.TOKEN as string, process.env.PREFIX as string);
-let password = "FHRCLM60izje1F2y";
 
 import { getDB } from "./database/mongo.js";
 
